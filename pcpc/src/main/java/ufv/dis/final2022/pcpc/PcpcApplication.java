66e8eb629cea7f9acad488ef2bc534d841ef9c75
tanusp <7736022@alumnos.ufv.es>
2022-06-12 16:46:39 +0200

    ArchivoSpriing

---
+++ pcpc/src/main/java/ufv/dis/final2022/pcpc/PcpcApplication.java
@@ -0,0 +1,13 @@
+package ufv.dis.final2022.pcpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcpcApplication.class, args);
+	}
+
+}
---
+++ pcpc/src/test/java/ufv/dis/final2022/pcpc/PcpcApplicationTests.java
@@ -0,0 +1,13 @@
+package ufv.dis.final2022.pcpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
